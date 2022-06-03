75f971c527e8e52d89d737ddad8524f4f0aeb338
Seth David Gyimah <esec26@gmail.com>
2022-06-03 05:29:46 +0000

    0x02 python import module

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import magic_calculation_102
+
+
+def magic_calculatrice(a, b):
+    pass
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    sys.path.append("variable_load_5")
+    import variable_load_5 as i
+    print(i.a)
