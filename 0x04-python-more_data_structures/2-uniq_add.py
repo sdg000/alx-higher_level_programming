36d090b29bf61a7a0c1ea9f7fabfd595d6943123
Seth David Gyimah <esec26@gmail.com>
2022-06-10 05:06:56 +0000

    revised

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
+
 def uniq_add(my_list=[]):
-    if my_list:
-        black_list = []
-        som = 0
-        for n in my_list:
-            if n not in black_list:
-                som += n
-                black_list.append(n)
-        return(som)
+    result = 0
+    for x in set(my_list):
+        result += x
+    return result
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
+
 def multiply_by_2(a_dictionary):
-    if a_dictionary:
-        dic = {}
-        for k in a_dictionary.keys():
-            dic[k] = a_dictionary[k] * 2
-    return(dic)
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
